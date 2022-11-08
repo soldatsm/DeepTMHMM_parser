1d1bc6d5848a6c5abc65073049f612718385798a
soldatsm <soldatsm@yandex.ru>
2022-11-08 20:39:57 +0300

    update

--- DeepTMHMM_parser.py
+++ DeepTMHMM_parser.py
@@ -1,30 +1,69 @@
-# TODO Парсер результатов DeepTMHMM в xls\csv таблицу
-#TODO Сделать так чтобы он либо добавлял к имеющейся таблице инфу из парсера \\
-# тогда нужно чтобы он соотносил результаты TM по именам генов
-# TODO  Либо отдельную таблицу где имена генов из его аутпута
-#TODO Так же добавить опцию с тем чтобы делать или не делать столбце с TM топологией
-# если делать что нужно запрашивать второй аутпут TM
-
 import argparse
 import pandas as pd
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('input', help='Path to .3line file')
-parser.add_argument('output')
-parser.add_argument('-simple')
-parser.add_argument('-topology')
+parser.add_argument('output', help='Path for output file')
+parser.add_argument('-delimiter', help='Choose delimiter if output file 1 - tab, 2 - comma',
+                    choices=[1, 2], type=int)
+parser.add_argument('-simple', help='Make table only from 3line file ')
+parser.add_argument('-topology', help='Make topology reformation'
+                                      'in addition it is a path to TMRs.gff3')
+
 
 args = parser.parse_args()
 
 
-def simple_parser(line_data, topol=None):
+def simple_parser(line_data):
+    table_data = pd.DataFrame()
     splited_list = [line_data[i:i + 3] for i in range(0, len(line_data) + 1, 3)]
-    table_data = pd.DataFrame({'Type':splited_list})
+    for i in splited_list:  # remove empty lists
+        if len(i) == 0:
+            splited_list.remove(i)
+    for idx, val in enumerate(splited_list):
+        table_data.loc[idx, 'Name'] = val[0].split(' | ')[0].replace('>','')
+        table_data.loc[idx, 'Type'] = val[0].split(' | ')[1]
+        table_data.loc[idx, 'AA'] = val[1]
+        table_data.loc[idx, 'Topology'] = val[2]
     return table_data
 
-def parser_merj():
-    pass
+
+def topol_parser(topology_file):
+
+    plain_list = []
+    nested_list = []
+    accum = []
+    protein_name = []
+    tm_composition = []
+
+    with open(topology_file, 'r') as topol_read_file:
+        text = topol_read_file.readlines()
+        for i in text:
+            if '##' in i:
+                continue
+            else:
+                plain_list.append(i.replace('# ', ''))
+    for j in plain_list:
+        if '//' in j:
+            nested_list.append(accum)
+            accum = []
+        else:
+            accum.append(j)
+    for structure in nested_list:
+        accum = []
+        protein_name.append(structure[0].split('\t')[0].split(' ')[0])
+
+        for data in structure[2::]:
+            processed_composition = data.split('\t')
+            accum.append(f'{processed_composition[1][0].lower()}{processed_composition[2]}-{processed_composition[3]}')
+        tm_composition.append(' '.join(accum).strip())
+
+    table_for_merge = pd.DataFrame({
+            'IDs': protein_name,
+            'TM_composition': tm_composition})
+
+    return table_for_merge
 
 
 if __name__ == '__main__':
@@ -36,19 +75,34 @@ if __name__ == '__main__':
         line_data = [i.strip('\n') for i in line_data]
         read_file.close()
 
-    if args.topology is not None:
-        with open(rf'{args.topology}', 'r') as topol_file:
-            topol_data = read_file.readlines()
-
-        topol_data = topol_data[4::]
-
-        new_data = []
-
-        for i in topol_data:
-            new_data.append(i.strip("'").strip('\n'))
-            if i == '\n':
-                new_data.pop()
-
+    out_table = simple_parser(line_data)
 
     if args.simple is not None:
-        simple_parser(line_data)
+        if args.delimiter == 1:
+
+            out_table.to_csv(rf'{args.output}',
+                             index=False, sep='\t')
+        elif args.delimiter == 2:
+
+            out_table.to_csv(rf'{args.output}',
+                             index=False)
+
+    elif args.topology is not None:
+        print(1)
+        topology_table = topol_parser(args.topology)
+
+        topology_table.to_csv(rf'{args.output}_tolp.csv')
+
+        out_table = out_table.merge(topology_table, how='inner',
+                                    right_on='IDs', left_on='Name')
+
+        out_table = out_table.iloc[:, [0, 1, 2, 5]]
+
+        if args.delimiter == 1:
+
+            out_table.to_csv(rf'{args.output}',
+                             index=False, sep='\t')
+        elif args.delimiter == 2:
+
+            out_table.to_csv(rf'{args.output}',
+                             index=False)
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Read Me
 
+## Description
+
+This is parser of data from [DeepTMHMM](https://dtu.biolib.com/DeepTMHMM/)
+
+It takes file `.3line` and\or `.gff3 ` to create table.
+
+This table consist of next colums:
+* Name -- protein id according to 3line and gff3
+* Type -- protein type (TM, GLOB,SP or TM+SP)
+* AAs -- amino acid sequence
+* TM_composition -- protein structure in format **o1-12 t13-20 s21-30**, where o - ouside localisation of AA and number of such AA, t - membrane localisation, s - signal part.
+
+## Dependence 
+
+*`pandas`
+
+*`argparse`
+
+## Help message
+To print help message use flag -h
+
+`DeepTMHMM_parser.py -h`
